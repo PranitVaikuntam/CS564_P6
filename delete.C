01e6635ab5042bc44b591162e2104c69ba405a17
Jesse Mendez <mendezj2740@gmail.com>
2025-12-15 21:05:15 -0600

    delete.C and select.C implemented

--- delete.C
+++ delete.C
@@ -1,7 +1,6 @@
 #include "catalog.h"
 #include "query.h"
-
-
+ 
 /*
  * Deletes records from a specified relation.
  *
@@ -16,11 +15,93 @@ const Status QU_Delete(const string & relation,
 		       const Datatype type, 
 		       const char *attrValue)
 {
-// part 6
-return OK;
-
-
-
+    Status status;
+    HeapFileScan *hfs;
+    RID rid;
+    
+    // Check if this is an unconditional delete (no WHERE clause)
+    if (attrName.empty()) {
+        // Unconditional delete - delete all records
+        hfs = new HeapFileScan(relation, status);
+        if (status != OK) {
+            return status;
+        }
+        
+        // Start an unconditional scan (pass 0 for offset, 0 for length)
+        status = hfs->startScan(0, 0, STRING, NULL, EQ);
+        if (status != OK) {
+            delete hfs;
+            return status;
+        }
+        
+        // Delete all records
+        while (hfs->scanNext(rid) == OK) {
+            status = hfs->deleteRecord();
+            if (status != OK) {
+                delete hfs;
+                return status;
+            }
+        }
+        
+        delete hfs;
+        return OK;
+    }
+    
+    // Conditional delete - need to get attribute information from catalog
+    AttrDesc attrDesc;
+    status = attrCat->getInfo(relation, attrName, attrDesc);
+    if (status != OK) {
+        return status;
+    }
+    
+    // Create HeapFileScan object
+    hfs = new HeapFileScan(relation, status);
+    if (status != OK) {
+        return status;
+    }
+    
+    // Convert attrValue to the appropriate type and start the scan
+    int intValue;
+    float floatValue;
+    
+    switch (type) {
+        case INTEGER:
+            intValue = atoi(attrValue);
+            status = hfs->startScan(attrDesc.attrOffset, attrDesc.attrLen, 
+                                   INTEGER, (char*)&intValue, op);
+            break;
+            
+        case FLOAT:
+            floatValue = atof(attrValue);
+            status = hfs->startScan(attrDesc.attrOffset, attrDesc.attrLen, 
+                                   FLOAT, (char*)&floatValue, op);
+            break;
+            
+        case STRING:
+            status = hfs->startScan(attrDesc.attrOffset, attrDesc.attrLen, 
+                                   STRING, attrValue, op);
+            break;
+            
+        default:
+            delete hfs;
+            return BADCATPARM;
+    }
+    
+    if (status != OK) {
+        delete hfs;
+        return status;
+    }
+    
+    // Delete all matching records
+    while (hfs->scanNext(rid) == OK) {
+        status = hfs->deleteRecord();
+        if (status != OK) {
+            delete hfs;
+            return status;
+        }
+    }
+    
+    // Clean up and return
+    delete hfs;
+    return OK;
 }
-
-
--- select.C
+++ select.C
@@ -1,5 +1,9 @@
 #include "catalog.h"
 #include "query.h"
+#include "stdio.h"
+#include "stdlib.h"
+
+
 
 
 // forward declaration
@@ -9,7 +13,7 @@ const Status ScanSelect(const string & result,
 			const AttrDesc *attrDesc, 
 			const Operator op, 
 			const char *filter,
-			const int reclen);
+			const int reclen); 
 
 /*
  * Selects records from the specified relation.
@@ -28,13 +32,82 @@ const Status QU_Select(const string & result,
 {
    // Qu_Select sets up things and then calls ScanSelect to do the actual work
     cout << "Doing QU_Select " << endl;
+	
+	// checks for valid 
+	Status status;
+	int reclen = 0;
+
+	if (projCnt <= 0){
+		return OK;
+	}
+
+	AttrDesc* attrDescArray = new AttrDesc[projCnt];
+	int recLen = 0;
+
+	for (int i = 0; i<projCnt; i++){
+		status = attrCat->getInfo(projNames[i].relName, projNames[i].attrName, attrDescArray[i]);
+		if(status !=OK){
+			delete[] attrDescArray;
+			return status;
+		}
+		reclen += attrDescArray[i].attrLen;
+	}
+
+	AttrDesc attrDesc;
+	const AttrDesc* attrDescPtr = NULL;
+
+	int prevInt = 0;
+	float prevFloat = 0.0;
+	char *prevString = NULL;
+	const char *filter = NULL;
+
+	if(attr!=NULL){
+		if (attrValue == NULL){
+			delete[] attrDescArray;
+			return BADSCANPARM;
+		}
+
+		status = attrCat->getInfo(attr->relName, attr->attrName, attrDesc);
+		if(status != OK){
+			delete[] attrDescArray;
+			return status;
+		}
+
+		attrDescPtr = &attrDesc;
+		switch(attrDesc.attrType){
+			case INTEGER:
+				prevInt = atoi(attrValue);
+				filter = (const char*)&prevInt;
+				break;
+			case FLOAT:
+				prevFloat = (float)atof(attrValue);
+				filter = (const char*)&prevFloat;
+				break;
+			case STRING:
+				prevString = new char[attrDesc.attrLen];
+				memset(prevString, 0, attrDesc.attrLen);
+				strncpy(prevString, attrValue, attrDesc.attrLen);
+				filter = prevString;
+				break;
+			default:
+				delete[] attrDescArray;
+				return BADSCANPARM;
+		}
+	}
+
+	status = ScanSelect(result, projCnt, attrDescArray, attrDescPtr, op, filter, reclen);
+	if(prevString != NULL){
+		delete [] prevString;
+	}
+
+	delete[] attrDescArray;
+	return status;
 
 }
 
 
 const Status ScanSelect(const string & result, 
-#include "stdio.h"
-#include "stdlib.h"
+
 			const int projCnt, 
 			const AttrDesc projNames[],
 			const AttrDesc *attrDesc, 
@@ -44,5 +117,66 @@ const Status ScanSelect(const string & result,
 {
     cout << "Doing HeapFileScan Selection using ScanSelect()" << endl;
 
+	Status status;
+	InsertFileScan resultRel(result, status);
+	if(status != OK){
+		return status;
+	}
+
+
+	HeapFileScan heapFileScan(string(projNames[0].relName), status);
+	if(status != OK){
+		return status;
+	}
+
+
+	if(attrDesc != NULL){
+		status = heapFileScan.startScan(attrDesc->attrOffset, attrDesc->attrLen,
+										(Datatype)attrDesc->attrType, filter, op);
+	} else{
+		status = heapFileScan.startScan(0,0,STRING,NULL, EQ);
+	}
+
+	if(status!=OK){
+		return status;
+	}
+
+	char *outputData = new char[reclen]; 
+	Record outputRec;
+	outputRec.data = (void*)outputData;
+	outputRec.length = reclen;
+
+	RID rid;
+	while((status = heapFileScan.scanNext(rid)) == OK){
+		Record rec;
+		status = heapFileScan.getRecord(rec);
+
+		if(status != OK){
+			delete[] outputData;
+			heapFileScan.endScan();
+			return status;
+		}
+
+		// project the desired attributes
+		int offset=0;
+		for(int i=0; i<projCnt; i++){
+			memcpy((char*)outputRec.data + offset, (char*)rec.data + projNames[i].attrOffset,
+				   projNames[i].attrLen);
+			offset += projNames[i].attrLen;
+		}
+		RID outRid;
+		status = resultRel.insertRecord(outputRec, outRid);
+		if(status != OK){
+			delete[] outputData;
+			heapFileScan.endScan();
+			return status;
+		}
+	}
+
+	delete[] outputData;
+	heapFileScan.endScan();
+
+
+	return OK;
 
 }
